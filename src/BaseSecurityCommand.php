620bc66f2ddbe1b2f8aac759168619b153587aec
Thomas Hoffmann <t.hoffmann.1979@googlemail.com>
2025-07-07 11:37:29 +0200

    add basic tests

--- src/BaseSecurityCommand.php
+++ src/BaseSecurityCommand.php
@@ -3,9 +3,46 @@
 namespace Tualo\Office\Security;
 
 use Garden\Cli\Cli;
+use Tualo\Office\Basic\TualoApplication as App;
+use Tualo\Office\Basic\PostCheck;
+use Tualo\Office\Basic\Path;
+use GuzzleHttp\Client;
 
 class BaseSecurityCommand
 {
+
+    public static function checkURIAccess($file): bool
+    {
+        if (App::configuration('security', 'base_url', false) === false) {
+            PostCheck::formatPrintLn(['red'], "base_url is not set configuration");
+        } else {
+            $url =  App::configuration('security', 'base_url');
+            $backendPath =  App::configuration('security', 'backend_path');
+            if ($backendPath == false) {
+                PostCheck::formatPrintLn(['red'], "backend_path is not set configuration");
+            } else {
+                try {
+                    $client = new Client(
+                        [
+                            'base_uri' =>  $url,
+                            'timeout'  => 1.0,
+                        ]
+                    );
+                    $response = $client->get($backendPath . '/composer.json', []);
+                    $code = $response->getStatusCode(); // 200
+
+                    if ($code == 200) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } catch (\Exception $e) {
+                    return false;
+                }
+            }
+        }
+        return false;
+    }
 
     public static function perform(string $cmdString, string $clientName)
     {
